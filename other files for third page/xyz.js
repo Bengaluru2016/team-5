b8e804e216eed88981405767d1df565c7ce81e63
SaiKrishnanThyagarajan <tsaikrish0612@gmail.com>
2016-07-09 19:09:40 +0530

    other files for third page

---
+++ other files for third page/xyz.js
@@ -0,0 +1,12 @@
+$('.ratings_stars').hover(
+    // Handles the mouseover
+    function() {
+        $(this).prevAll().andSelf().addClass('ratings_over');
+        $(this).nextAll().removeClass('ratings_vote'); 
+    },
+    // Handles the mouseout
+    function() {
+        $(this).prevAll().andSelf().removeClass('ratings_over');
+        set_votes($(this).parent());
+    }
+);
